800450566266fb67dc174e9b1f96de5abfb2cb84
chevisutton <cheviksutton@gmail.com>
2018-07-16 17:10:04 -0600

    Notes added, clean-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; //materialDesign
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'; // materialDesign
 import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 
--- src/app/name-conf.pipe.ts
+++ src/app/name-conf.pipe.ts
@@ -8,22 +8,26 @@ export class NameConfPipe implements PipeTransform {
   transform(name, names): any {
 
     let counter = 0;
+    // counter is equal to zero everytime this is run
 
     for (let i = 1; i < names.length; i++) {
+      // i equals one, so it doesn't count first name in array
       if (names[i] === name) {
+        // iterates through names and compares it to the name passed into pipe
         counter++;
+        // counter goes up for every match of the name passed in
       }
-
     }
 
     name = name + counter;
+    // name always equals name + counter
     if (name.endsWith('0')) {
-      return name.substring(0, name.length - 1;
+      // if name ends with zero, remove zero and return name
+      return name.substring(0, name.length - 1);
     } else {
       return name;
+    //  return name with counter which shows place of name
+      }
     }
-
-
-  }
 
 }
